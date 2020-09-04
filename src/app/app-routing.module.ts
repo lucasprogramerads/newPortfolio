4c15c9c1bca295ad474c855c376e496635f4dc46
lucasprogrammerads <lucas.oliveira@grupolife.med.br>
2020-09-04 20:45:59 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContatosComponent } from './pages/contatos/contatos.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent
+  },
+  {
+    path: 'contatos',
+    component: ContatosComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContatosComponent } from './pages/contatos/contatos.component';
+import { NavbarComponent } from './parts/navbar/navbar.component';
+import { FooterComponent } from './parts/footer/footer.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContatosComponent,
+    NavbarComponent,
+    FooterComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
